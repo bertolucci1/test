a92f95787a9d5caf34075b00b34bcbff02d3d201
Elias Costilla <eliasezecos@gmail.com>
2022-08-03 22:11:21 -0300

    first commit

---
+++ precache-manifest.c20433b4dbb4250899dcaf0dac73c8b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0ec5ba1a881fd0e8830781fb0179a2",
+    "url": "/bertolucci1/test.git/index.html"
+  },
+  {
+    "revision": "a9392b3a2bdb8fc6c9ab",
+    "url": "/bertolucci1/test.git/static/css/main.bf0095ea.chunk.css"
+  },
+  {
+    "revision": "9e76de35d71e578643ac",
+    "url": "/bertolucci1/test.git/static/js/2.4ee44813.chunk.js"
+  },
+  {
+    "revision": "a9392b3a2bdb8fc6c9ab",
+    "url": "/bertolucci1/test.git/static/js/main.4d0f961f.chunk.js"
+  },
+  {
+    "revision": "a480ef4b53f3c48817b5",
+    "url": "/bertolucci1/test.git/static/js/runtime-main.31f47763.js"
+  }
+]);
